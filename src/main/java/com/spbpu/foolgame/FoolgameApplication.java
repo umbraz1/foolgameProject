ee668a4565a52f04ab1b09c4ab71dd5ed4b7e815
umbraz1 <zinsirotm@gmail.com>
2022-11-17 18:48:06 +0300

    init

---
+++ src/main/java/com/spbpu/foolgame/FoolgameApplication.java
@@ -0,0 +1,13 @@
+package com.spbpu.foolgame;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FoolgameApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FoolgameApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/spbpu/foolgame/FoolgameApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spbpu.foolgame;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FoolgameApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
